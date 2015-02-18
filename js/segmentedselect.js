68b5cd7a8341171a6c2565cfa891b6209538ecd3
Onur Köse <kose.onur@gmail.com>
2015-02-18 23:19:21 +0100

    first js

---
+++ js/segmentedselect.js
@@ -0,0 +1,19 @@
+(function (factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        // Register as an anonymous AMD module:
+        define([
+            'jquery',
+            'jquery.ui.widget'
+        ], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS:
+        factory(
+            require('jquery'),
+            require('./vendor/jquery.ui.widget')
+        );
+    } else {
+        // Browser globals:
+        factory(window.jQuery);
+    }
+}
