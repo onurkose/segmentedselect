928e5125092d2f4737560689a42bb570d8f17c55
Onur Köse <kose.onur@gmail.com>
2015-02-22 16:33:24 +0100

    first commit
    This is first working version of my idea. Simply, give all the chain of
    select boxes, define URLs for each segment, define default selected
    option values for each segments and that’s it. It doesn’t matter two or
    more select boxes. As long as you define the URLs, it keep segmenting
    to next select box you defined and load content to the end.

--- js/segmentedselect.js
+++ js/segmentedselect.js
@@ -1,19 +1,89 @@
-(function (factory) {
-    'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // Register as an anonymous AMD module:
-        define([
-            'jquery',
-            'jquery.ui.widget'
-        ], factory);
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS:
-        factory(
-            require('jquery'),
-            require('./vendor/jquery.ui.widget')
-        );
-    } else {
-        // Browser globals:
-        factory(window.jQuery);
++function ($) { "use strict";
+    var options = {
+        segments    : [],
+        data        : {},
+        dataType    : 'json',
+        defaults    : [],
+        url         : []
+    };
+
+    var methods = {
+        init: function(userOptions){
+            options = $.extend(options, userOptions);
+
+            var segments = [];
+
+            $(options.segments).each(function(){
+                segments.push($('#' + this));
+            });
+
+            options.segments = [this].concat(segments);
+
+            $(options.segments).each(function(index, elm){
+                if (index + 1 == options.segments.length) {
+                    return false;
+                }
+
+                elm.on('change', function(e){
+                    methods.reset(index);
+
+                    if (_.isUndefined(options.url[index])) {
+                        return false;
+                    }
+
+                    options.data.key = $(this).val();
+
+                    $.post(options.url[index], options.data).done(function(d){
+                        methods.decorate(d, index + 1);
+                    });
+                })
+
+                if (!_.isUndefined(options.defaults[index])) {
+                    elm.find('option').each(function(){
+                        if ($(this).val() == options.defaults[index]) {
+                            $(this).attr('selected', true);
+                        }
+                    });
+                }
+            });
+            
+            return this;
+        },
+
+        reset: function(i){
+            var elm = options.segments[i + 1];
+
+            if (!_.isUndefined(elm)) {
+                elm.find('option').remove();
+            }
+        },
+
+        decorate: function(data, i){
+            var elm = options.segments[i];
+
+            if (data.total > 0 && !_.isUndefined(elm)) {
+                $(data.rows).each(function(){
+                    var opt = $('<option>').attr({
+                        value: this.id
+                    }).html(this.text).appendTo(elm);
+
+                    if (!_.isUndefined(options.defaults[i])) {
+                        if (this.id == options.defaults[i]) {
+                            opt.attr('selected', true);
+                        }
+                    }
+                });
+            }
+        }
+    };
+
+    $.fn.segmentThese = function(methodOrOptions) {
+        if ( methods[methodOrOptions] ) {
+            return methods[ methodOrOptions ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof methodOrOptions === 'object' || ! methodOrOptions ) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method ' +  methodOrOptions + ' does not exist on jQuery.tooltip' );
+        }
     }
-}
+}(window.jQuery);
